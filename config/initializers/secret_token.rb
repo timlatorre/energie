672673744201a37ca713e33eea20e6c1dbb360d8
Tim LaTorre <timlatorre@gmail.com>
2012-12-06 11:55:54 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Energie::Application.config.secret_token = '58d8801b8a04673facf2657b6a339a0a182d0f5b41666a4395a502e30340855dcc628b867e78ab3272ae52cb0e14593804814a9e9a4060ff553c1b80ae30527e'
